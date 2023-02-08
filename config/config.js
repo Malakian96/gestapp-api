a0e75e618027e7847b83828c0f68bba9045e708e
AlvaroArpal <alvaroarpal@gmail.com>
2023-02-08 22:59:34 +0100

    Fix enviroment sequelize

---
+++ config/config.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+
+module.exports = {
+  development: {
+    username: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DB,
+    host: process.env.HOST,
+    dialect: process.env.DIALECT,
+  },
+  test: {
+    username: "postgres",
+    password: "root",
+    database: "test",
+    host: "127.0.0.1",
+    dialect: "postgres",
+  },
+  production: {
+    username: "root",
+    password: null,
+    database: "database_production",
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
+};
